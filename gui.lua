2f6081e66f28e256743c8c8021d889231d6307f0
f0nez3 <104220491+FantZarYT@users.noreply.github.com>
2024-11-21 18:19:50 +0300

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,153 @@
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cueshut/saves/main/criminality%20paste%20ui%20library'))()
+
+-- // Window \\ --
+local window = library.new('j0nk1ck is so hot', 'j0nk1ck')
+
+-- // Tabs \\ --
+local tab = window.new_tab('rbxassetid://4483345998')
+
+-- // Sections \\ --
+local section = tab.new_section('Cool things')
+local section1 = tab.new_section('Games things')
+
+-- // Sector \\ --
+local sector = section.new_sector('Main FE', 'Left')
+local sector1 = section.new_sector('Other', 'Right')
+local sector2 = section1.new_sector('Fisch', 'Left')
+local sector3 = section1.new_sector('Doors', 'Right')
+local sector4 = section1.new_sector('MM2', 'Left')
+local sector5 = section1.new_sector('BABFT', 'Right')
+local sector6 = section1.new_sector('JailBreak', 'Left')
+local sector7 = section1.new_sector('For smth', 'Right')
+
+-- // Elements \\ -- (Type, Name, State, Callback)
+local button = sector.element('Button', 'Nameless Admin', nil, function()
+   loadstring(game:HttpGet("https://github.com/FilteringEnabled/NamelessAdmin/blob/main/Source?raw=true"))()
+end)
+
+local button = sector.element('Button', 'Simple Spy', nil, function()
+   loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+end)
+
+local button = sector.element('Button', 'Dark Dex v4', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/DannyH3103/scripts/main/Hydrogen_DEXV4"))()
+end)
+
+local button = sector.element('Button', 'Attach to part', nil, function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/YvJPhFNb"))()
+end)
+
+local button = sector.element('Button', 'Invis Troll', nil, function()
+   loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+end)
+
+local button = sector.element('Button', 'Telekinesis', nil, function()
+   loadstring(game:HttpGet('https://pastebin.com/raw/07GGb4Dk'))()
+end)
+
+local button = sector.element('Button', 'Unachored gui', nil, function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/WkZwcGjf", true))()
+end)
+
+local button = sector1.element('Button', 'Skibidi rizz emote', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua", true))()
+end)
+
+local button = sector1.element('Button', 'RO:TX', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/RO-TX/refs/heads/main/Main.lua"))()
+end)
+
+local button = sector1.element('Button', 'Lag Switch', nil, function()
+   loadstring(game:HttpGet("https://paste.gg/p/anonymous/d3492bd620bd4c46b54680ac80b0fa0d/files/161ddd9c992c4ceba3c298458d45188a/raw", true))()
+end)
+
+local button = sector1.element('Button', 'BlackHole Unachored', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/FantZarYT/da/refs/heads/main/daa"))()
+end)
+
+local button = sector1.element('Button', 'FE Ball', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/FantZarYT/da/refs/heads/main/daaa"))()
+end)
+
+local button = sector1.element('Button', 'Sniper', nil, function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/Sniper.lua"))()
+end)
+
+local button = sector2.element('Button', 'Random hub python', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/Python/main/Main.lua"))()
+end)
+
+local button = sector2.element('Button', 'Best autofarm', nil, function()
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/Lythicals/script/main/Fisch.lua")()
+end)
+
+local button = sector2.element('Button', 'Goomba hub', nil, function()
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/JustLevel/goombahub/refs/heads/main/fisch.lua")()
+end)
+
+local button = sector2.element('Button', 'Goomba hub prem', nil, function()
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/JustLevel/goombahub/refs/heads/main/fischprem.lua")()
+end)
+
+local button = sector2.element('Button', 'Goomba hub keyless', nil, function()
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/JustLevel/goombahub/refs/heads/main/fischkeyless.lua")()
+end)
+
+local button = sector3.element('Button', 'MSPaint', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
+end)
+
+local button = sector3.element('Button', 'Idk working farm coins hallow', nil, function()
+   local ReplicatedStorage = game:GetService("ReplicatedStorage")
+   local Remotes = ReplicatedStorage.RemotesFolder
+
+   Remotes.CollectCandy:FireServer(100)
+end)
+
+local button = sector3.element('Button', 'Mini Map v2', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/FantZarYT/da/refs/heads/main/mini%20map%20doors"))()
+end)
+
+local button = sector3.element('Button', 'Anticheat bypass old', nil, function()
+   game:GetService("Players").LocalPlayer.PlayerGui.MainUI.ItemShop:Destroy()
+require(game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).freemouse = false
+game:GetService("ReplicatedStorage").ClientModules.EntityModules.Void:Destroy()
+end)
+
+local button = sector3.element('Button', 'Jeff the killer plusie', nil, function()
+local tool = game:GetObjects("rbxassetid://13069619857")[1]
+tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+local button = sector3.element('Button', '*BOOOOOOOOOOOOM*', nil, function()
+local tool = game:GetObjects("rbxassetid://12018248")[1]
+tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+local button = sector3.element('Button', 'A-1000', nil, function()
+loadstring(game:HttpGet("https://pastebin.com/raw/7Shu1JxY"))()
+end)
+
+local button = sector4.element('Button', 'XHubRandom', nil, function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
+end)
+
+local button = sector4.element('Button', 'AutoFarm', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBossScript/BeeconHub/main/BeeconHub"))()
+end)
+
+local button = sector5.element('Button', 'Nameless Hub', nil, function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/XRoLLu/UWU/main/BUILD%20A%20BOAT%20FOR%20TREASURE.lua'))()
+end)
+
+local button = sector5.element('Button', 'Candy Farm', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/refs/heads/main/babft-candy-farm"))()
+end)
+
+local button = sector5.element('Button', 'N-word Hub/Stealer', nil, function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/goofyahh-hub/Build-A-Boat-With-Blocks-/refs/heads/main/Main"))()
+end)
+
+local button = sector6.element('Button', 'Project Auto Rob', nil, function()
+   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7540ba23dbc2a9228591964a91244237.lua"))()
+end)
